342a104c69641ecc55c26b72bb0ac703cd373a45
julien.ollier <julien.ollier@berger-levrault.fr>
2016-02-29 16:57:56 +0100

    Initialisation projet Maven Spring

---
+++ src/main/java/com/floremipy/FloremipyApplication.java
@@ -0,0 +1,12 @@
+package com.floremipy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloremipyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloremipyApplication.class, args);
+	}
+}
