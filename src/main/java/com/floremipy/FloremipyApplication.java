5fe3a29550d308165f353b6a68d3555cd9c05f05
julien.ollier <julien.ollier@berger-levrault.fr>
2016-03-01 14:55:45 +0100

    Bonne chance

--- src/main/java/com/floremipy/FloremipyApplication.java
+++ src/main/java/com/floremipy/FloremipyApplication.java
@@ -1,12 +1,8 @@
 package com.floremipy;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class FloremipyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FloremipyApplication.class, args);
+		//TODO
 	}
 }
--- src/test/java/com/floremipy/FloremipyApplicationTests.java
+++ src/test/java/com/floremipy/FloremipyApplicationTests.java
@@ -1,18 +1,14 @@
 package com.floremipy;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = FloremipyApplication.class)
-@WebAppConfiguration
 public class FloremipyApplicationTests {
-
+	
 	@Test
 	public void contextLoads() {
+		assertTrue(true);
 	}
 
 }
