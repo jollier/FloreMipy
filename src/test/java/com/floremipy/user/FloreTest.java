365f6279e7bdf8a9579147a887c19aa869179456
Christelle <christelle.gonzalez@berger-levrault.fr>
2016-03-03 17:04:43 +0100

    ajout section After dans FloreTest + assert ...

--- src/test/java/com/floremipy/user/FloreTest.java
+++ src/test/java/com/floremipy/user/FloreTest.java
@@ -1,5 +1,7 @@
 package com.floremipy.user;
 
+import static org.junit.Assert.*;
+
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -7,23 +9,29 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.floremipy.user.dao.UserDao;
 import com.floremipy.user.dto.UserDto;
 
+import junit.framework.Assert;
+
 
 public class FloreTest {
 	
 	private final static String PERSISTENCE_UNIT_NAME = "floremipyuser";
-	public static EntityManagerFactory emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-	public static EntityManager em = emf.createEntityManager();
-	public static EntityTransaction transaction = em.getTransaction();
+	public static EntityManagerFactory emf;
+	public static EntityManager em;
+	public static EntityTransaction transaction;
 
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		em =  emf.createEntityManager();
+		transaction =  em.getTransaction();
 	}
 
 	
@@ -35,11 +43,19 @@ public class FloreTest {
 		for(UserDto a : resultUser){
 			System.out.println("result test FindAllUsers : " +a);
 		}	
+		assertNotEquals(resultUser.size(), 0);
 	}
 	
 	@Test
 	public void testFindFirstUser() {	
 		User firstUser = em.find(User.class, new Long(1));
 		System.out.println("FirstUser :" + firstUser.toString());
+		assertNotNull(firstUser);
+	}
+	
+	@AfterClass
+	public static void setUpAfterClass() throws Exception {
+	em.close();
+	emf.close();
 	}
 }
