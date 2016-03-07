387305b14f6ad2e2abb69566ccb59d730fa75c04
Christelle <christelle.gonzalez@berger-levrault.fr>
2016-03-08 00:22:09 +0100

    ajout tests FloreModelCustomerTest

--- src/test/java/com/floremipy/model/customer/FloreModelCustomerTest.java
+++ src/test/java/com/floremipy/model/customer/FloreModelCustomerTest.java
@@ -3,6 +3,8 @@ package com.floremipy.model.customer;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -13,7 +15,14 @@ import org.junit.Test;
 
 import com.floremipy.model.Adress;
 import com.floremipy.model.Customer;
+import com.floremipy.model.article.dao.ArticleDao;
+import com.floremipy.model.article.dao.IArticleDao;
+import com.floremipy.model.article.dto.ArticleDto;
+import com.floremipy.model.article.dto.ArticleLightDto;
+import com.floremipy.model.customer.dao.IModelCustomerDao;
 import com.floremipy.model.customer.dao.ModelCustomerDao;
+import com.floremipy.model.customer.dto.CustomerDto;
+import com.floremipy.model.customer.dto.CustomerLightDto;
 import com.floremipy.user.dto.UserDto;
 
 public class FloreModelCustomerTest {
@@ -42,14 +51,53 @@ public class FloreModelCustomerTest {
 	}
 	
 	
+//	@Test
+//	public void testCustomerDaoSave() {
+//		ModelCustomerDao modelCustomerDao = new ModelCustomerDao();
+//		Adress adress = new Adress ("rue du village","31000","Toulouse");
+//		Customer customer = new Customer ("test.teste@mail.com","Teste","Test","(555)555-5555",adress);
+//		modelCustomerDao.CustomerDaoSave(customer);				
+//	}
+	
+	
 	@Test
-	public void testCustomerDaoSave() {
-		ModelCustomerDao modelCustomerDao = new ModelCustomerDao();
-		Adress adress = new Adress ("rue du village","31000","Toulouse");
-		Customer customer = new Customer ("test.teste@mail.com","Teste","Test","(555)555-5555",adress);
-		modelCustomerDao.CustomerDaoSave(customer);
-		
-		
+	public void findAllCustomers(){
+		IModelCustomerDao modelDao = new ModelCustomerDao();
+		List<CustomerDto> result = modelDao.findAllCustomers();
+		for(CustomerDto c : result){
+			System.out.println("result test findAllCustomers "+ c);
+		}
+		assertTrue(result.size() > 0);	
+	}
+	
+	@Test
+	
+	public void testFindArticleById() {
+		int id = 1;
+		IModelCustomerDao modelDao = new ModelCustomerDao();
+		CustomerDto result = modelDao.findCustomerById(id);
+		System.out.println("result test findCustomerById : " +result.toString());
+		assertEquals(id,result.getId());
+	
+	}
+	
+	@Test
+	public void testFindAllCustomersLight() {
+		IModelCustomerDao modelDao = new ModelCustomerDao();
+		List<CustomerLightDto> result = modelDao.findAllCustomersLight();
+		for(CustomerLightDto a : result){
+			System.out.println("result test FindAllArticlesLight : " +a);
+		}	
+		assertTrue(result.size() > 0);
+	}
+	
+	@Test
+	public void testFindCustomerLightById() {
+		int id = 1;
+		IModelCustomerDao modelDao = new ModelCustomerDao();
+		CustomerLightDto result = modelDao.findCustomerLightById(id);
+		System.out.println("result test FindCustomerLightById : " +result.toString());
+		assertEquals(id,result.getId());
 	}
 	
 
