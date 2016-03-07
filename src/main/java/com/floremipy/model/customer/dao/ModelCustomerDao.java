cc01a55d4d75e2902cdaddfaca07efd582ff555c
Christelle <christelle.gonzalez@berger-levrault.fr>
2016-03-07 15:55:49 +0100

    ajout CustomerDto

--- src/main/java/com/floremipy/model/customer/dao/ModelCustomerDao.java
+++ src/main/java/com/floremipy/model/customer/dao/ModelCustomerDao.java
@@ -1,6 +1,23 @@
 package com.floremipy.model.customer.dao;
 
-public class ModelCustomerDao {
+import java.io.Serializable;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class ModelCustomerDao implements Serializable{
+	
+	private final static String PERSISTENCE_UNIT_NAME = "FloreMipyWeb";
+	private static EntityManagerFactory emf;	
+	private static EntityManager em;
+	
+	public ModelCustomerDao() {
+		emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		em = emf.createEntityManager();
+	}
+	
+	
 	
 
 }
--- src/main/java/com/floremipy/model/customer/dto/CustomerDto.java
+++ src/main/java/com/floremipy/model/customer/dto/CustomerDto.java
@@ -1,5 +1,87 @@
 package com.floremipy.model.customer.dto;
 
-public class CustomerDto {
+import com.floremipy.model.Adress;
 
+public class CustomerDto {
+	
+	private int id;
+	private String name;
+	private String firstname;
+	private String phone;
+	private String email;
+	private Adress adress;
+	
+	
+	public CustomerDto() {
+		super();
+	}
+
+	
+	
+	public CustomerDto(int id, String name, String firstname, String phone, String email, Adress adress) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.firstname = firstname;
+		this.phone = phone;
+		this.email = email;
+		this.adress = adress;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Adress getAdress() {
+		return adress;
+	}
+
+	public void setAdress(Adress adress) {
+		this.adress = adress;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "CustomerDto [id=" + id + ", name=" + name + ", firstname=" + firstname + ", phone=" + phone + ", email="
+				+ email + ", adress=" + adress + "]";
+	}
+	
+	
 }
