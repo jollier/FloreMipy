939b9137553736fb5e13dbb15da21d430a684eaf
julien.ollier <julien.ollier@berger-levrault.fr>
2016-03-07 22:03:25 +0100

    rename du serviceUser et de l'interface serviceUser

---
+++ src/main/java/com/floremipy/user/service/IUserService.java
@@ -0,0 +1,18 @@
+package com.floremipy.user.service;
+
+import java.util.List;
+
+import com.floremipy.user.User;
+
+public interface IUserService {
+	
+	public List<User> getAll();
+	
+	public User getUser();
+	
+	public User getUserById();
+	
+	public boolean delUser(User user);
+	
+	public void save(User user);
+}
