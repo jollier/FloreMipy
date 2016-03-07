c55f579ae2d9a51db6e52fa6e41a260594a0c3f6
Christelle <christelle.gonzalez@berger-levrault.fr>
2016-03-07 16:18:48 +0100

    ajout findAllCustomers et findCustomerById

--- src/main/java/com/floremipy/model/customer/dao/ModelCustomerDao.java
+++ src/main/java/com/floremipy/model/customer/dao/ModelCustomerDao.java
@@ -2,15 +2,15 @@ package com.floremipy.model.customer.dao;
 
 
 import java.io.Serializable;
-
-
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
+import javax.persistence.Query;
 
 import com.floremipy.model.Customer;
+import com.floremipy.model.customer.dto.CustomerDto;
 
 public class ModelCustomerDao {
 
@@ -30,5 +30,28 @@ public class ModelCustomerDao {
 		em.persist(customer);
 		em.getTransaction().commit();
 	}
+	
+	public List<CustomerDto> findAllCustomers(){
+		String requete = 
+				"SELECT NEW com.floremipy.model.dto.CustomerDto(" + 
+		"a.id , a.name, a.firstname, a.phone, a.email, a.adress)" +
+						"From Customer ORDER BY name";
+		Query query = null;
+		query=em.createQuery(requete, CustomerDto.class);		
+		return (List<CustomerDto>)query.getResultList();
+		
+	}
+	
+	public CustomerDto findCustomerById(int id){
+		String requete = "SELECT New com.floremipy.dto.CustomerDto(" +
+				"a.id , a.name, a.firstname, a.phone, a.email, a.adress)" +
+				"From Customer WHERE a.id = id";
+		
+		return null;
+		
+		
+	}
+	
+	
 
 }
