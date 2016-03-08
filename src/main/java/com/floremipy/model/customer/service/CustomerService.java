48af6f86a2ed37bf89e3a6b7ceba24b02b3863ca
sylvain.rabois <sylvain.rabois@berger-levrault.fr>
2016-03-08 16:15:19 +0100

    correction findall service customer

--- src/main/java/com/floremipy/model/customer/service/CustomerService.java
+++ src/main/java/com/floremipy/model/customer/service/CustomerService.java
@@ -45,8 +45,7 @@ public class CustomerService implements ICustomerService {
 
 	@Override
 	public List<CustomerDto> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return customerDao.findAllCustomers();
 	}
 
 }
