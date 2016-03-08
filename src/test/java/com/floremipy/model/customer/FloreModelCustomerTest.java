2bf5d3700e2df6d45372b586ef84a0ab227d1765
FrodonSaquet <alexandre.redon@berger-levrault.fr>
2016-03-08 16:45:41 +0100

    FloreModelCustomerTest

--- src/test/java/com/floremipy/model/customer/FloreModelCustomerTest.java
+++ src/test/java/com/floremipy/model/customer/FloreModelCustomerTest.java
@@ -15,6 +15,7 @@ import org.junit.Test;
 
 import com.floremipy.model.Adress;
 import com.floremipy.model.Customer;
+import com.floremipy.model.Version;
 import com.floremipy.model.article.dao.ArticleDao;
 import com.floremipy.model.article.dao.IArticleDao;
 import com.floremipy.model.article.dto.ArticleDto;
@@ -26,92 +27,92 @@ import com.floremipy.model.customer.dto.CustomerLightDto;
 import com.floremipy.user.dto.UserDto;
 
 public class FloreModelCustomerTest {
-	
-	private final static String PERSISTENCE_UNIT_NAME = "FloreMipyWeb";
-	public static EntityManagerFactory emf;
-	public static EntityManager em;
-	public static EntityTransaction transaction;
+       
+       private final static String PERSISTENCE_UNIT_NAME = "FloreMipyWeb";
+       public static EntityManagerFactory emf;
+       public static EntityManager em;
+       public static EntityTransaction transaction;
 
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		int versionBaseUser = 5;
-		emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-		em =  emf.createEntityManager();
-		Version version = em.find(Version.class, 1);
-		
-		if (version.getVersion() != versionBaseUser) {
-			System.out.println("");
-			System.out.println("");
-			System.out.println("********************************");
-			System.out.println("**Version de la BDD floremipi incorrecte**");
-			System.out.println("********************************");
-			System.out.println("Veuillez l'importer depuis srv-dev/PARTAGES/FloreMipy-2016-02-29/FloreMipy/FloreMipiInit.sql");
-			
-			System.exit(1);
-		}
-		
-	}
-	
-	
-	@Test
-	public void testIdLastCustomer() {
-		ModelCustomerDao modelCustomerDao = new ModelCustomerDao();
-		int id;
-		id = modelCustomerDao.FindIdLastCustomer();
-		System.out.println("MaxId : " + id);
-		assertEquals(id,8);
-	}
-	
-	
-	@Test
-	public void testCustomerDaoSave() {
-		ModelCustomerDao modelCustomerDao = new ModelCustomerDao();
-		Adress adress = new Adress ("rue du village","31000","Toulouse");
-		Customer customer = new Customer ("test.teste@mail.com","Teste","Test","(555)555-5555",adress);
-		modelCustomerDao.CustomerDaoSave(customer);				
-	}
-	
-	
-	@Test
-	public void findAllCustomers(){
-		IModelCustomerDao modelDao = new ModelCustomerDao();
-		List<CustomerDto> result = modelDao.findAllCustomers();
-		for(CustomerDto c : result){
-			System.out.println("result test findAllCustomers "+ c);
-		}
-		assertTrue(result.size() > 0);	
-	}
-	
-	@Test
-	
-	public void testFindArticleById() {
-		int id = 1;
-		IModelCustomerDao modelDao = new ModelCustomerDao();
-		CustomerDto result = modelDao.findCustomerById(id);
-		System.out.println("result test findCustomerById : " +result.toString());
-		assertEquals(id,result.getId());
-	
-	}
-	
-	@Test
-	public void testFindAllCustomersLight() {
-		IModelCustomerDao modelDao = new ModelCustomerDao();
-		List<CustomerLightDto> result = modelDao.findAllCustomersLight();
-		for(CustomerLightDto a : result){
-			System.out.println("result test FindAllArticlesLight : " +a);
-		}	
-		assertTrue(result.size() > 0);
-	}
-	
-	@Test
-	public void testFindCustomerLightById() {
-		int id = 1;
-		IModelCustomerDao modelDao = new ModelCustomerDao();
-		CustomerLightDto result = modelDao.findCustomerLightById(id);
-		System.out.println("result test FindCustomerLightById : " +result.toString());
-		assertEquals(id,result.getId());
-	}
-	
+       @BeforeClass
+       public static void setUpBeforeClass() throws Exception {
+             int versionBaseUser = 5;
+             emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+             em =  emf.createEntityManager();
+             Version version = em.find(Version.class, 1);
+             
+             if (version.getVersion() != versionBaseUser) {
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("********************************");
+                    System.out.println("**Version de la BDD floremipi incorrecte**");
+                    System.out.println("********************************");
+                    System.out.println("Veuillez l'importer depuis srv-dev/PARTAGES/FloreMipy-2016-02-29/FloreMipy/FloreMipiInit.sql");
+                    
+                    System.exit(1);
+             }
+             
+       }
+       
+       /*
+       @Test
+       public void testNameLastCustomer() {
+             ModelCustomerDao modelCustomerDao = new ModelCustomerDao();
+             String name;
+             name = modelCustomerDao.FindNameLastCustomer();
+             System.out.println("Nom : " + name);
+             assertEquals(name,"Test");
+       }
+       */
+       
+       @Test
+       public void testCustomerDaoSave() {
+             ModelCustomerDao modelCustomerDao = new ModelCustomerDao();
+             Adress adress = new Adress ("rue du village","31000","Toulouse");
+             Customer customer = new Customer (0,"test.teste@mail.com","Teste","Test","(555)555-5555",adress);
+             modelCustomerDao.CustomerDaoSave(customer);                        
+       }
+       
+       
+       @Test
+       public void findAllCustomers(){
+             IModelCustomerDao modelDao = new ModelCustomerDao();
+             List<CustomerDto> result = modelDao.findAllCustomers();
+             for(CustomerDto c : result){
+                    System.out.println("result test findAllCustomers "+ c);
+             }
+             assertTrue(result.size() > 0);    
+       }
+       
+       @Test
+       
+       public void testFindArticleById() {
+             int id = 1;
+             IModelCustomerDao modelDao = new ModelCustomerDao();
+             CustomerDto result = modelDao.findCustomerById(id);
+             System.out.println("result test findCustomerById : " +result.toString());
+             assertEquals(id,result.getId());
+       
+       }
+       
+       @Test
+       public void testFindAllCustomersLight() {
+             IModelCustomerDao modelDao = new ModelCustomerDao();
+             List<CustomerLightDto> result = modelDao.findAllCustomersLight();
+             for(CustomerLightDto a : result){
+                    System.out.println("result test FindAllArticlesLight : " +a);
+             }      
+             assertTrue(result.size() > 0);
+       }
+       
+       @Test
+       public void testFindCustomerLightById() {
+             int id = 1;
+             IModelCustomerDao modelDao = new ModelCustomerDao();
+             CustomerLightDto result = modelDao.findCustomerLightById(id);
+             System.out.println("result test FindCustomerLightById : " +result.toString());
+             assertEquals(id,result.getId());
+       }
+       
 
 }
