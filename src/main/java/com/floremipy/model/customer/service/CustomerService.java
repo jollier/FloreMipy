857952b015e67f0e075c72c0d370d6f0510c8528
sylvain.rabois <sylvain.rabois@berger-levrault.fr>
2016-03-08 15:25:53 +0100

    interface Customer extraite

--- src/main/java/com/floremipy/model/customer/service/CustomerService.java
+++ src/main/java/com/floremipy/model/customer/service/CustomerService.java
@@ -1,16 +1,19 @@
 package com.floremipy.model.customer.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.floremipy.model.Customer;
 import com.floremipy.model.customer.dao.IModelCustomerDao;
 import com.floremipy.model.customer.dto.CustomerDto;
+import com.floremipy.model.customer.dto.CustomerLightDto;
 import com.floremipy.user.User;
 import com.floremipy.user.dao.IUserDao;
 import com.floremipy.user.dao.UserDao;
 import com.floremipy.user.dto.UserDto;
 
-public class CustomerService {
+public class CustomerService implements ICustomerService {
 	
 	@Autowired
 	IModelCustomerDao customerDao;
@@ -39,6 +42,42 @@ public class CustomerService {
 			//customerDto = customerDao.findCustomerById(userDto.getIdcustomer());
 		}
 		return customerDto;
+	}
+
+	@Override
+	public int FindIdLastCustomer() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void CustomerDaoSave(Customer customer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<CustomerDto> findAllCustomers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CustomerDto findCustomerById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<CustomerLightDto> findAllCustomersLight() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CustomerLightDto findCustomerLightById(int id) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
--- src/main/java/com/floremipy/model/customer/service/ICustomerService.java
+++ src/main/java/com/floremipy/model/customer/service/ICustomerService.java
@@ -1,5 +1,19 @@
 package com.floremipy.model.customer.service;
 
+import java.util.List;
+
+import com.floremipy.model.Customer;
+import com.floremipy.model.customer.dto.CustomerDto;
+import com.floremipy.model.customer.dto.CustomerLightDto;
+
 public interface ICustomerService {
+	
+	int FindIdLastCustomer();
+	void CustomerDaoSave (Customer customer);
+	List<CustomerDto> findAllCustomers();
+	CustomerDto findCustomerById(int id);
+	List<CustomerLightDto> findAllCustomersLight();
+	CustomerLightDto findCustomerLightById(int id);
+	
 
 }
