24a4781d03177ba32cb23d66570739347410eff2
Olivier <olivier.chamayou@berger-levrault.fr>
2016-03-09 17:09:20 +0100

    Rajout ProductLight dans le WebService

--- src/main/java/com/floremipy/model/article/webservice/ModelWebService.java
+++ src/main/java/com/floremipy/model/article/webservice/ModelWebService.java
@@ -2,6 +2,7 @@ package com.floremipy.model.article.webservice;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -25,43 +26,59 @@ public class ModelWebService {
 	@Autowired
 	IArticleService articleService;
 	private final AtomicLong counter = new AtomicLong();
+	private List<ProductLight> list;
 
-	@RequestMapping(value = "/product/get")
+	@RequestMapping(value = "/Product/get")
 	public ArticleLightDto article(@RequestParam(value="id", defaultValue="0") int id, @RequestParam String category,
 			@RequestParam String description, @RequestParam String name, @RequestParam(value="quantityInStock", defaultValue="0") int quantityInStock,
 			@RequestParam(value="value", defaultValue="0") BigDecimal value) {
 		return new ArticleLightDto(id,category,description, name, quantityInStock, value);
-		
+
 
 	}
-	
-	@ResponseBody @RequestMapping(value = "/productlist")
-	public List<ArticleLightDto> articlelist(@RequestParam(value="id", defaultValue="0") int id, @RequestParam(value="category", defaultValue="Arbre") String category,
+
+	@ResponseBody @RequestMapping(value = "/Product/list")
+	public List<ProductLight> articlelist(@RequestParam(value="id", defaultValue="0") int id, @RequestParam(value="category", defaultValue="Arbre") String category,
 			@RequestParam(value="description", defaultValue="Vert") String description, @RequestParam(value="name", defaultValue="ZouliNarbre") String name, @RequestParam(value="quantityInStock", defaultValue="0") int quantityInStock,
 			@RequestParam(value="value", defaultValue="0") BigDecimal value) {
-	
-		
-		
+
+
+
 		List<ArticleLightDto> result = articleService.findAllDto();
-		String jsonInString = "";
+		list = new ArrayList<ProductLight>();
+		for (ArticleLightDto articleLightDto : result) {
+
+			ProductLight pl = new ProductLight();
+			pl.setId(articleLightDto.getId());
+			pl.setName(articleLightDto.getName());
+			pl.setCategory(articleLightDto.getCategory());
+			pl.setQuantityInStock(articleLightDto.getQuantityInStock());
+			pl.setAlertLotMature(0);
+			list.add(pl);
+		}
+
+
+
+
+		/*String jsonInString = "";
 		ObjectMapper mapper = new ObjectMapper();
 
 		try {
-			
+
 			// Convert object to JSON string
-			jsonInString = mapper.writeValueAsString(result+" AlerteArticle {Test rajout info supp}");
+			jsonInString = mapper.writeValueAsString(result);
 			System.out.println(jsonInString);
-	
+
 		} catch (JsonGenerationException e) {
 			e.printStackTrace();
 		} catch (JsonMappingException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-			
-		return result;
-		
+		}*/
+
+		return list;
+
 
 	}
 }
