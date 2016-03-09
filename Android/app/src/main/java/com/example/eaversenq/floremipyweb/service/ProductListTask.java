9941001444c75a0f5d05377eafbb65d31bdb1309
eaversenq <emmanuel.aversenq@berger-levrault.fr>
2016-03-09 17:15:20 +0100

    eave : connexion web service

---
+++ Android/app/src/main/java/com/example/eaversenq/floremipyweb/service/ProductListTask.java
@@ -0,0 +1,12 @@
+package com.example.eaversenq.floremipyweb.service;
+
+import android.os.AsyncTask;
+
+public class ProductListTask extends AsyncTask<Void,Void,Void> {
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        return null;
+    }
+
+}
