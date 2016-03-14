1112f12b87dee4ade1f1cedfd8185cc2e3509eb6
FrodonSaquet <alexandre.redon@berger-levrault.fr>
2016-03-14 09:57:03 +0100

    ajout @service sur class ArticleInProgressService

--- src/main/java/com/floremipy/model/articleinprogress/service/ArticleInProgressService.java
+++ src/main/java/com/floremipy/model/articleinprogress/service/ArticleInProgressService.java
@@ -3,10 +3,12 @@ package com.floremipy.model.articleinprogress.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.floremipy.model.articleinprogress.dao.IArticleInProgressDao;
 import com.floremipy.model.articleinprogress.dto.ArticleInProgressDto;
 
+@Service
 public class ArticleInProgressService implements IArticleInProgressService {
 
 	@Autowired
