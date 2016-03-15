8e15f5e60207b17d22f422289c5a62f86202204a
FrodonSaquet <alexandre.redon@berger-levrault.fr>
2016-03-15 16:31:05 +0100

    ajout desciptions dans article

--- sql/FloreMipi.sql
+++ sql/FloreMipi.sql
@@ -25,11 +25,7 @@ CREATE TABLE IF NOT EXISTS `adress` (
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=latin1;
 
-<<<<<<< HEAD
--- Export de données de la table floremipi.adress : ~14 rows (environ)
-=======
 -- Export de données de la table floremipi.adress : ~8 rows (environ)
->>>>>>> developpement
 DELETE FROM `adress`;
 /*!40000 ALTER TABLE `adress` DISABLE KEYS */;
 INSERT INTO `adress` (`Id`, `Location`, `ZipCode`, `City`) VALUES
@@ -54,27 +50,23 @@ CREATE TABLE IF NOT EXISTS `article` (
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=82 DEFAULT CHARSET=latin1;
 
--- Export de données de la table floremipi.article : ~17 rows (environ)
+-- Export de données de la table floremipi.article : ~13 rows (environ)
 DELETE FROM `article`;
 /*!40000 ALTER TABLE `article` DISABLE KEYS */;
 INSERT INTO `article` (`Id`, `Name`, `Description`, `Category`, `Imgsrc`, `QuantityInStock`) VALUES
-	(1, 'Câdre en boule', '', 'Arbres', 'img/cadre_en_boule.jpg', 10),
-	(2, 'Sapin', '', 'Arbres', 'img/Sapin.JPG', 10),
-	(3, 'Epinette bleue', '', 'Arbres', 'img/Epinette_bleue.jpg', 10),
-	(4, 'Chêne', '', 'Arbres', 'img/Chene.jpg', 10),
-	(5, 'Erable argenté', '', 'Arbres', 'img/Erable_argente.jpg', 10),
-	(6, 'Herbe à puce', '', 'Herbacées', 'img/Herbe_a_puce.jpg', 10),
-	(7, 'Poirier', '', 'Fruits', 'img/Poirier.jpg', 10),
-	(8, 'Catalpa', '', 'Arbres', 'img/Catalpa.jpg', 10),
-	(9, 'Pommier', '', 'Fruits', 'img/Pommier.jpg', 10),
-	(10, 'Génévrier', '', 'Arbustes', 'img/Genevrier.jpg', 10),
-<<<<<<< HEAD
-	(11, 'Abricotier rustique', '', 'Fruits', 'img/Abricotier.jpg', 10);
-=======
-	(11, 'Abricotier rustique', '', 'Fruits', 'img/Abricotier.jpg', 10),
-	(82, 'Cactus', ' ', 'Cactus', 'img/Dancing_cactus.gif', 12),
-	(83, 'Try and Pray', ' ', 'Arbres', 'img/Try_and_Pray.jpg', 1);
->>>>>>> developpement
+	(1, 'Câdre en boule', 'Super cadre de Noël avec des boules (de Noël)', 'Arbres', 'img/cadre_en_boule.jpg', 10),
+	(2, 'Sapin', 'Ca sent le ...', 'Arbres', 'img/Sapin.JPG', 10),
+	(3, 'Epinette bleue', 'Epi net', 'Arbres', 'img/Epinette_bleue.jpg', 10),
+	(4, 'Chêne', 'SM bienvenu', 'Arbres', 'img/Chene.jpg', 10),
+	(5, 'Erable argenté', 'Tabernacle', 'Arbres', 'img/Erable_argente.jpg', 10),
+	(6, 'Herbe à puce', 'Gratt Gratt', 'Herbacées', 'img/Herbe_a_puce.jpg', 10),
+	(7, 'Poirier', 'Jean-Marie (RIP)', 'Fruits', 'img/Poirier.jpg', 10),
+	(8, 'Catalpa', '????', 'Arbres', 'img/Catalpa.jpg', 10),
+	(9, 'Pommier', 'Jean-Marie (private joke)', 'Fruits', 'img/Pommier.jpg', 10),
+	(10, 'Génévrier', 'hips', 'Arbustes', 'img/Genevrier.jpg', 10),
+	(11, 'Abricotier rustique', 'J\'adore sucer un abricot pendant des heures', 'Fruits', 'img/Abricotier.jpg', 10),
+	(82, 'Cactus', 'Aie ça pique', 'Cactus', 'img/Dancing_cactus.gif', 12),
+	(83, 'Try and Pray', 'Même pas cap !', 'Arbres', 'img/Try_and_Pray.jpg', 1);
 /*!40000 ALTER TABLE `article` ENABLE KEYS */;
 
 -- Export de la structure de la table floremipi. articleinprogress
@@ -89,7 +81,7 @@ CREATE TABLE IF NOT EXISTS `articleinprogress` (
   CONSTRAINT `articleinprogress_ibfk_1` FOREIGN KEY (`IdArticle`) REFERENCES `article` (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=latin1;
 
--- Export de données de la table floremipi.articleinprogress : ~4 rows (environ)
+-- Export de données de la table floremipi.articleinprogress : ~1 rows (environ)
 DELETE FROM `articleinprogress`;
 /*!40000 ALTER TABLE `articleinprogress` DISABLE KEYS */;
 INSERT INTO `articleinprogress` (`Id`, `Quantity`, `StartDate`, `ReleaseDate`, `IdArticle`) VALUES
@@ -109,11 +101,7 @@ CREATE TABLE IF NOT EXISTS `customer` (
   CONSTRAINT `customer_ibfk_1` FOREIGN KEY (`IdAdress`) REFERENCES `adress` (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
 
-<<<<<<< HEAD
--- Export de données de la table floremipi.customer : ~14 rows (environ)
-=======
 -- Export de données de la table floremipi.customer : ~8 rows (environ)
->>>>>>> developpement
 DELETE FROM `customer`;
 /*!40000 ALTER TABLE `customer` DISABLE KEYS */;
 INSERT INTO `customer` (`Id`, `Name`, `FirstName`, `Phone`, `Email`, `IdAdress`) VALUES
@@ -281,7 +269,7 @@ CREATE TABLE IF NOT EXISTS `price` (
   CONSTRAINT `price_ibfk_1` FOREIGN KEY (`IdArticle`) REFERENCES `article` (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=78 DEFAULT CHARSET=latin1;
 
--- Export de données de la table floremipi.price : ~17 rows (environ)
+-- Export de données de la table floremipi.price : ~11 rows (environ)
 DELETE FROM `price`;
 /*!40000 ALTER TABLE `price` DISABLE KEYS */;
 INSERT INTO `price` (`Id`, `IdArticle`, `Value`, `Date`) VALUES
