7a07fa97a25eccea2463e8b2c394551147646b9d
Christelle <christelle.gonzalez@berger-levrault.fr>
2016-03-15 16:32:11 +0100

    ajout quiSommesNous

--- src/main/java/com/floremipy/web/Accueil.java
+++ src/main/java/com/floremipy/web/Accueil.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.floremipy.model.article.dto.ArticleDto;
 import com.floremipy.model.article.service.IArticleService;
@@ -34,5 +35,12 @@ public class Accueil {
 
 		return "accueil";
 	}
+	
+		
+		@RequestMapping("/quiSommesNous")
+		
+		public ModelAndView test(){
+			return new ModelAndView("quiSommesNous");
+		}
 
 }
